cadc825b4c47b7c854a287d001d794d82d66b23d
muhammadaa1i <muhammadaalin01@gmail.com>
2024-12-16 00:24:41 +0500

    ast7

--- src/Components/About/Carousel2.jsx
+++ src/Components/About/Carousel2.jsx
@@ -66,7 +66,7 @@ export default function Carousel2() {
       <Slider
         {...settings}>
         {images.map((image, index) => (
-          <div key={index} className='slide mt-6 ml-10 text-center  max-p830:max-w-[750px] max-p470:max-w-[400px]'>
+          <div key={index} className='slide mt-6 ml-4 text-center  max-p830:max-w-[750px] max-p470:max-w-[400px]'>
             <img src={image} alt={`Slide ${index + 1}`} className='w-[350px] h-[300px] rounded-lg mx-auto mx-2 max-p830:mx-auto max-p470:mx-[-30px] max-p456:max-w-[300px] max-p456:max-h-[250px] max-p456:mx-[0px] max-p360:max-w-[250px] max-p360:mx-[30px]' />
             <h2 className='w-[350px] text-3xl font-semibold mx-auto mx-2 max-p830:mx-auto max-p470:mx-[-30px]'>{text[index]}</h2>
           </div>
